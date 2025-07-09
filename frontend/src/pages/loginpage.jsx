8e575d652300c5af8353a5d4fd38b8f663d81dec
Ahamed Atheef <ahdatheef451@gmail.com>
2025-07-09 18:37:14 +0530

    create register page

--- frontend/src/pages/loginpage.jsx
+++ frontend/src/pages/loginpage.jsx
@@ -5,14 +5,46 @@ import toast from "react-hot-toast";
 import { FaFacebookF,  } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 
-
-
 export default function Loginpage() {
+
+    /* Login page */
+    const [showLogin, setShowLogin] = useState(true);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [showPassword, setShowPassword] = useState(false);
     const navigate = useNavigate(); // Load page smoothly
 
+    /* Register page */
+
+    const [fname,setFname] = useState("")
+    const [lname,setLname] = useState("")
+    const [femail,setFemail] = useState("")
+    const [fpassword,setFPassword] = useState("")
+    const [number,setNumber] = useState("")
+
+    function Register(){
+        axios.post(import.meta.env.VITE_BACKEND_URL+ "/api/users",{
+        firstName:fname, 
+        lastName:lname, 
+        email:femail,
+        password:fpassword, 
+        phone:number
+        })
+        .then((res)=>{
+            console.log(res.data)
+            if(res.data.message === "Not Saved"){
+                return toast.error("Failed To Create An Account")
+            }
+            toast.success("Successfully Created An Account")
+            navigate("/")
+
+        }).catch((error)=>{
+            console.log(error)
+            toast.error("Can't To Create An Account and Try Again ")
+
+        }) 
+    }
+    // Login page
     function login() {
         axios.post(import.meta.env.VITE_BACKEND_URL + "/api/users/login", {
             email: email,
@@ -37,14 +69,15 @@ export default function Loginpage() {
         });
     }
 
-    return (
-        <div className="w-full h-screen bg-[url('/login.jpg')] bg-cover bg-center bg-no-repeat grid place-content-center">
-            <div className="w-[70rem] h-[620px] backdrop-blur-sm flex gap-[110px] rounded-[30px] custom-shadow">
+    return ( 
+        /* Login page  */
+        <div className="w-full h-screen bg-[url('/login.jpg')] bg-cover bg-center bg-no-repeat  grid place-content-center relative">
+            <div className={`absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[70rem] h-[620px] backdrop-blur-sm flex gap-[110px] rounded-[30px] custom-shadow transition-all duration-500 ease-in-out ${showLogin ? 'block' : 'hidden'}`}>
                 <div className="w-[calc(100%-50%)] rounded-l-[30px] text-white flex flex-col items-center relative">
                     <div className="absolute z-10 text-center px-[20px] pt-[8rem] flex flex-col items-center gap-[3rem]">
-                        <h1 className=" text-4xl font-bold ">Heading</h1>
+                        <h1 className=" text-4xl font-bold ">Welcome Back</h1>
                     <span className="text-gray-400 text-[18px]">Experience luxury with our skin-friendly cosmetics, carefully crafted to enhance your natural beauty, nourish your skin, and give you the radiant confidence to shine every single day. Discover elegance in every shade and glow like never before.</span>
-                    <Link className=" w-[10rem] text-2xl text-center pb-[5px] rounded-2xl  bg-gradient-to-r from-blue-500 to-cyan-400 shadow-[0_4px_10px_rgba(0,0,0,0.6)] hover:shadow-[0_6px_15px_rgba(0,0,0,0.7)] transition-shadow" to="/">Get Started</Link>
+                    <Link className=" w-[10rem] text-2xl text-center pb-[5px] rounded-2xl  bg-gradient-to-r from-blue-500 to-cyan-400 shadow-[0_4px_10px_rgba(0,0,0,0.6)] hover:shadow-[0_6px_15px_rgba(0,0,0,0.7)] transition-shadow mt-[11rem]" to="/">Get Started</Link>
 
                     </div>
                     <img src="/cream.jpg" alt="" className="w-full h-full absolute rounded-l-[30px]"/>
@@ -91,7 +124,7 @@ export default function Loginpage() {
                 {/* ✅ Form ends here */}
 
                 <span className="mt-[20px] text-[17px] text-[#232933]">
-                    Don't have an account? <Link className="text-blue-900" to="/registerpage">SignUp</Link> from here
+                    Don't have an account? <button className="text-blue-900 cursor-pointer" onClick={() => setShowLogin(false)}>SignUp</button> from here
                 </span>
                 <span className="mt-[5px] text-[17px] text-[#161b24] text-center">OR Continue With</span>
                 <div className="flex flex-col gap-[20px] mt-[10px]">
@@ -106,6 +139,81 @@ export default function Loginpage() {
                     </div>
                 </div>
             </div>
+            
+            {/* Register Page */}
+
+            <div className={`absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[70rem] h-[620px] backdrop-blur-sm flex gap-[110px] rounded-[30px] custom-shadow z-10 transition-all duration-500 ease-in-out ${showLogin ? 'hidden' : 'block'}`}>
+                <div className="w-[calc(100%-50%)] rounded-l-[30px] text-white flex flex-col items-center relative">
+                    <div className="absolute z-10 text-center px-[20px] pt-[8rem] flex flex-col items-center gap-[3rem]">
+                        <h1 className=" text-4xl font-bold">Create an Acoount</h1>
+                    <span className="text-gray-400 text-[18px]">Experience luxury with our skin-friendly cosmetics, carefully crafted to enhance your natural beauty, nourish your skin, and give you the radiant confidence to shine every single day. Discover elegance in every shade and glow like never before.</span>
+                    <Link className=" w-[10rem] text-2xl text-center pb-[5px] rounded-2xl  bg-gradient-to-r from-blue-500 to-cyan-400 shadow-[0_4px_10px_rgba(0,0,0,0.6)] hover:shadow-[0_6px_15px_rgba(0,0,0,0.7)] transition-shadow mt-[11rem]" to="/">Get Started</Link>
+
+                    </div>
+                    <img src="/cream.jpg" alt="" className="w-full h-full absolute rounded-l-[30px]"/>
+                </div>
+                <div className="flex flex-col items-center text-center">
+                    <div className="mt-[30px] mb-[20px]">
+                    <h1 className="text-[#2c7bb0] font-bold text-[35px] ">Create a new account</h1>
+                    <h2 className="text-[20px] text-[#343a46]">Create your beauty account</h2>
+                    </div>
+                
+                <form onSubmit={(e) => { e.preventDefault(); Register() ; }} className="flex flex-col gap-[28px] pt-[20px] relative">
+                    <input
+                        type="text"
+                        placeholder="First name"
+                        onChange={(e)=> setFname(e.target.value)}
+                        className="w-[350px] h-[40px] text-[18px] px-3 border border-[#416793] rounded-[10px] text-black bg-transparent outline-none placeholder-black"
+                    />
+
+                    <input
+                        type="text"
+                        placeholder="Last name"
+                        onChange={(e)=> setLname(e.target.value)}
+                        className="w-[350px] h-[40px] text-[18px] px-3 border border-[#416793] rounded-[10px] text-black bg-transparent outline-none placeholder-black"
+                    />
+
+                    <input
+                        type="text"
+                        inputMode="numeric"
+                        pattern="[0-9]*"
+                        maxLength={10}
+                        onChange={(e)=> setNumber(e.target.value)}
+                        placeholder="Phone number"
+                        onKeyPress={(e) => {
+                        if (!/[0-9]/.test(e.key)){
+                        e.preventDefault();}}}
+                        className="w-[350px] h-[40px] text-[18px] px-3 border border-[#416793] rounded-[10px] text-black bg-transparent outline-none placeholder-black"
+                    />
+                    
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        onChange={(e)=> setFemail(e.target.value)}
+                        className="w-[350px] h-[40px] text-[18px] px-3 border border-[#416793] rounded-[10px] text-black bg-transparent outline-none placeholder-black"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Password"
+                        onChange={(e)=> setFPassword(e.target.value)}
+                        className="w-[350px] h-[40px] text-[18px] border border-[#416793] px-3 rounded-[10px] text-black bg-transparent outline-none placeholder-black"
+                    />
+
+                    <button
+                        type="submit"
+                        className="w-[350px] h-[45px] rounded-[10px] text-white font-semibold bg-gradient-to-r from-blue-600 to-cyan-400 hover:opacity-80 transition duration-300 cursor-pointer text-[22px] mt-[15px]"
+                    >
+                        Submit
+                    </button>
+                </form>
+                {/* ✅ Form ends here */}
+
+                <span className="mt-[20px] text-[17px] text-[#232933]">
+                    All ready have an account? <button className="text-blue-900 cursor-pointer" onClick={() => setShowLogin(true)}>SignIn</button> from here
+                </span>
+                </div>
+            </div>
+
         </div>
     );
 }
